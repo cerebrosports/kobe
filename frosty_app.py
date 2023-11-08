f906469a659a6de7846c423a59e06929df908b7c
cerebrosports <144247550+cerebrosports@users.noreply.github.com>
2023-11-08 14:09:02 -0500

    Update frosty_app.py

--- frosty_app.py
+++ frosty_app.py
@@ -31,7 +31,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
         response = ""
         resp_container = st.empty()
         for delta in openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-4-1106-preview",
             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
             stream=True,
         ):
